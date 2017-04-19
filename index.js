3c0a737e58cb9c4de9f541d8384955230ebb80c9
jmjuanes <josemijuanes@gmail.com>
2017-04-19 16:18:04 +0200

    index.js: minor changes

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-//Exports the main channel
-module.exports.main = require('./lib/main.js');
-
-//Exports the render channel
-module.exports.render = require('./lib/render.js');
+module.exports = {};
